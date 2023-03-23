6e64f8936e3fb61d6fa89485f64462383739ac91
Nickolay Makushevskii <creywer@gmail.com>
2023-03-23 19:25:37 +0300

    2.3

--- app/src/main/java/com/grifalion/astontwo/MainActivity.kt
+++ app/src/main/java/com/grifalion/astontwo/MainActivity.kt
@@ -1,55 +1,74 @@
 package com.grifalion.astontwo
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.location.Location
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ShareCompat
 import com.grifalion.astontwo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var mCount = 0
-
-    companion object{
-        val KEY_FOR_SECOND_ACTIVITY = "countHello"
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        showToast()
-        countUp()
+        init()
     }
+    private fun init() = with(binding){
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString("keyInstance", binding.showCount.text.toString())
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        binding.showCount.text = savedInstanceState.getString("keyInstance")
-    }
-
-    private fun countUp() = with(binding){
-        buttonCount.setOnClickListener {
-            mCount++
-            if(showCount != null){
-                showCount.text = mCount.toString()
+        btnWebsite.setOnClickListener {
+            val url = edWebsite.text.toString()
+            val webpage = Uri.parse(url)
+            val intent = Intent(Intent.ACTION_VIEW, webpage)
+            if(intent.resolveActivity(packageManager) != null && edLocation.text.isNotEmpty()) {
+                startActivity(intent)
+            } else{
+                Toast.makeText(this@MainActivity,R.string.alarm_put_street,Toast.LENGTH_SHORT).show()
             }
+        }
+
+        btnLocation.setOnClickListener {
+            var loc = edLocation.text.toString()
+            var adressUri = Uri.parse("geo:0,0?q=" + loc)
+            val intent = Intent(Intent.ACTION_VIEW, adressUri)
+            try {
+                startActivity(intent)
+            } catch (e: Exception) {
+                Toast.makeText(this@MainActivity, R.string.alarm_put_street, Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+
+        btnShare.setOnClickListener {
+            val edShareText = edShare.text.toString()
+            val shareIntent = Intent().apply {
+                this.action = Intent.ACTION_SEND
+                this.putExtra(Intent.EXTRA_TEXT,edShareText)
+                this.type = "text/plain"
+            }
+            startActivity(shareIntent)
+        }
+
+        btnTakePicture.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(takePictureIntent,123)
         }
     }
 
-    private fun showToast(){
-        binding.buttonToast.setOnClickListener {
-            Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT).show()
-            val intent = Intent(this,SecondActivity::class.java)
-            val messageForActivity = binding.showCount.text.toString()
-            intent.putExtra(KEY_FOR_SECOND_ACTIVITY,messageForActivity)
-            startActivity(intent)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 123) {
+            val bmp = data?.extras?.get("data") as Bitmap
+            binding.imRes.setImageBitmap(bmp)
+
         }
     }
 }
